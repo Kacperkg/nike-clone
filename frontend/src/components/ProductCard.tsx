18b069e920cbb2aad62b987ea3394b0ed1b2a0ee
Kacper Gajdarski <gajdarski.kacper@gmail.com>
2026-02-27 04:19:57 +0000

    removed featured product card

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -4,7 +4,7 @@ import { type Product } from "@/lib/products"
 export default function ProductCard({ product, subhead }: { product: Product, subhead?: string }) { 
     return (
         <Link to="/$type/$productId" params={{ type: 'products', productId: product.id }} className="overflow-hidden w-full h-auto hover:scale-104 transition-transform rounded-md">
-              <img src={product.image} alt={product.name} className="object-cover" />
+              <img src={product.image} alt={product.name} className="object-cover w-full h-auto"/>
               <div className="w-full py-4 text-black">
                 <h1 className="text-lg font-semibold">{product.name}</h1>
                 {subhead && <h3 className="text-black/50">{subhead}</h3>}
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,6 +3,7 @@ import ImageContainer from '../components/Image-container'
 import { fetchProducts } from '@/lib/products'
 import { useQuery } from '@tanstack/react-query'
 import { type Product } from '../lib/products'
+import ProductCard from '@/components/ProductCard'
 
 export const Route = createFileRoute('/')({ component: App })
 
@@ -50,22 +51,10 @@ function FeaturedProducts() {
       {products && (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {products.map((product) => (
-            <FeaturedProductCard key={product.id} product={product} />
+            <ProductCard key={product.id} product={product} />
           ))}
         </div>
       )}
     </div>
-  )
-}
-
-function FeaturedProductCard({ product }: { product: Product }) {
-  return (
-    <Link to="/$type/$productId" params={{ type: 'products', productId: product.id }} className="overflow-hidden relative w-full h-auto hover:scale-104 transition-transform rounded-md">
-      <img src={product.image} alt={product.name} className="object-cover" />
-      <div className="absolute bottom-0 left-0 w-full p-4 text-black">
-        <h3 className="text-lg font-semibold">{product.name}</h3>
-        <p className="text-black/70">£{product.price}</p>
-      </div>
-    </Link>
   )
 }
